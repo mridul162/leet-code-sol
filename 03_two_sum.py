0694975367c29da52dd81f6285f72252c9362468
mridul162 <mridulbd75@gmail.com>
2025-11-17 12:57:18 +0600

    optimize bruteForce method

--- 03_two_sum.py
+++ 03_two_sum.py
@@ -3,13 +3,14 @@
 
 class Solution:
     def bruteForce(self, nums: list[int], target: int) -> list[int]:
-        for i in range(0,(len(nums)-1)):
-            for j in range (1, len(nums)):
-                if i!=j and nums[i]+nums[j] == target:
-                    return sorted([i,j])
+        for i in range(len(nums)-1):
+            for j in range (i+1, len(nums)):
+                if nums[i]+nums[j] == target:
+                    return [i,j]
+        return []
 
 
-nums = [2,7,11,15]
-target = 9
+nums = [3,7,2,4,4]
+target = 8
 sol = Solution()
 print(sol.bruteForce(nums, target))
