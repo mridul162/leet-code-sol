53797cacf11baf8f6c81d47f0dbb0510167bad0f
mridul162 <mridul162@gmail.com>
2025-11-20 22:02:34 +0600

    add comment explaining the code

--- 04_valid_palindrome.py
+++ 04_valid_palindrome.py
@@ -3,23 +3,23 @@
 
 class Solution:
     def reverseString(self, s: str) -> bool:
-        clean_text = ''.join(ch.lower() for ch in s if ch.isalnum())
-        return clean_text == clean_text [::-1]
+        clean_text = ''.join(ch.lower() for ch in s if ch.isalnum()) # filter non-alphanum and lowercase
+        return clean_text == clean_text [::-1]                       # check if string is equal to its reverse
     
     def twoPointers(self, s: str) -> bool:
         l, r= 0, len(s) - 1
 
         while l < r:
-            while l < r and not self.alphaNum(s[l]):
+            while l < r and not self.alphaNum(s[l]):                 # skip non-alphanumeric characters
                 l += 1
             while l < r and not self.alphaNum(s[r]):
                 r -= 1
-            if s[l].lower() != s[r].lower():
+            if s[l].lower() != s[r].lower():    
                 return False
             l, r = l + 1, r - 1
         return True
     
-    def alphaNum(self, c):
+    def alphaNum(self, c):                                           # check if character is alphanumeric
         return (ord('A') <= ord(c) <= ord('Z') or
                 ord('a') <= ord(c) <= ord('z') or
                 ord('0') <= ord(c) <= ord('9'))
